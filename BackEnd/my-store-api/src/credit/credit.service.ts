fb9bf5f2ee5619dd6464d67404b347bbf4a2e194
yassir63 <amamiyassir@gmail.com>
2022-12-25 14:18:05 +0100

    Added endpoints to Produits

--- BackEnd/my-store-api/src/credit/credit.service.ts
+++ BackEnd/my-store-api/src/credit/credit.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateCreditDto } from './dto/create-credit.dto';
 import { UpdateCreditDto } from './dto/update-credit.dto';
+import { fix_date } from 'src/shared_funcs/fix_date';
+import { PrismaService } from 'prisma/prisma.service';
+
 
 @Injectable()
 export class CreditService {
-  create(createCreditDto: CreateCreditDto) {
-    return 'This action adds a new credit';
+
+  constructor(private readonly prismaService: PrismaService){}
+  
+  async create(createCreditDto: CreateCreditDto) {
+    createCreditDto.Date_limite = fix_date(
+      createCreditDto.Date_limite,
+    );
+    try {
+      return await this.prismaService.achat_credit.create({
+      data: createCreditDto,
+      });
+    } catch (e) {
+      console.log("error") ;
+      return new HttpException(
+        'couldnt create this Credit',
+        HttpStatus.BAD_REQUEST, 
+      );
+    }
   }
 
   findAll() {
-    return `This action returns all credit`;
+    return this.prismaService.achat_credit.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} credit`;
+    return this.prismaService.achat_credit.findUnique({
+      where: { id_achat: id },
+    });
   }
 
-  update(id: number, updateCreditDto: UpdateCreditDto) {
-    return `This action updates a #${id} credit`;
+  update(id: number, createCreditDto: CreateCreditDto) {
+    createCreditDto.Date_limite = fix_date(
+      createCreditDto.Date_limite,
+    );
+
+    return this.prismaService.achat_credit.update({
+      where: {
+        id_achat: id,
+      },
+      data: createCreditDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} credit`;
+    return this.prismaService.achat_credit.delete({
+      where: {
+        id_achat: id,
+      },
+    });  
   }
 }
--- BackEnd/my-store-api/src/credit/dto/create-credit.dto.ts
+++ BackEnd/my-store-api/src/credit/dto/create-credit.dto.ts
@@ -1,1 +1,27 @@
-export class CreateCreditDto {}
+import {
+    IsNumber,
+    IsNotEmpty,
+    IsDateString,
+    IsString,
+  } from 'class-validator';
+  
+
+    // check this
+
+  export class CreateCreditDto {
+  
+  
+    @IsDateString()
+    @IsNotEmpty()
+    Date_limite: Date;
+
+    @IsString()
+    @IsNotEmpty()
+    nom_crediteur: string;
+  
+    @IsNumber()
+    @IsNotEmpty()
+    id_achat: number;
+  
+  }
+  
