2bd56b23c371ced4a2aef184c4ef628eae98f403
yassir63 <amamiyassir@gmail.com>
2022-12-29 13:47:34 +0100

    End Points Done !

---
+++ BackEnd/my-store-api/src/produits_achetes/produits_achetes/produits_achetes.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ProduitsAchetesService } from './produits_achetes.service';
+import { CreateProduitsAcheteDto } from './dto/create-produits_achete.dto';
+import { UpdateProduitsAcheteDto } from './dto/update-produits_achete.dto';
+
+@Controller('produits-achetes')
+export class ProduitsAchetesController {
+  constructor(private readonly produitsAchetesService: ProduitsAchetesService) {}
+
+  @Post()
+  create(@Body() createProduitsAcheteDto: CreateProduitsAcheteDto) {
+    return this.produitsAchetesService.create(createProduitsAcheteDto);
+  }
+
+  @Get(':id')
+  findAll(@Param('id') id: number) {
+    return this.produitsAchetesService.findAll(+id);
+  }
+
+  @Get('/:idA/:idP')
+  findOne(@Param('idA') idA: number , @Param('idP') idP: number) {
+    return this.produitsAchetesService.findOne( +idA , +idP );
+  }
+
+  @Patch('/:idA/:idP')
+  update(@Param('idA') idA: number , @Param('idP') idP: number, @Body() updateProduitsAcheteDto: UpdateProduitsAcheteDto) {
+    return this.produitsAchetesService.update(+idA , +idP, updateProduitsAcheteDto);
+  }
+
+  @Delete('/:idA/:idP')
+  remove(@Param('idA') idA: number, @Param('idP') idP: number) {
+    return this.produitsAchetesService.remove(+idA , +idP );
+  }
+}
